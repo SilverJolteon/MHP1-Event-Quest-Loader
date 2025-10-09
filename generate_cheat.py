dbc1f4a60a6b54e83a730e96683c0fda20545b38
Silver <silverchrysaetos@live.com>
2025-10-08 18:03:34 -0600

    Update generate_cheat.py

--- generate_cheat.py
+++ generate_cheat.py
@@ -2,7 +2,7 @@ from cwcheatio import CwCheatIO
 
 file = CwCheatIO("ULJM05066.ini")
 
-file.write(f"Event Quests v1.0")
+file.write(f"Event Quest Loader v1.0")
 file.write(f"_L 0x210ed6b4 0x2A620013\n") # slti v0,s3,0x3 -> slti v0,s3,0x13
 file.write(f"_L 0x210ee7f8 0x0a200800\n") # lw v1,0x0(v0) -> j 0x08802000
 file.write(f"_L 0x210ee7fc 0x00000000\n") # bnel v1,zero,0x098EE80C -> nop
